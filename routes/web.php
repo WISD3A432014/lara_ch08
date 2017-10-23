35e69f370745f52bb5f105ecc48ff86e8a92bdeb
Wu Hsiu Rong <mandy19970219@gm.student..ncut.edu.tw>
2017-10-23 17:49:01 +0800

    練習四_將參數做格式限制

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,11 @@ Route::get('student/{student_no}/score/{subject}', function ($student_no,$subjec
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
 });
+
+//練習四: 正規表達式限制參數_將參數做格式限制
+Route::get('student/{student_no}',function ($student_no){
+    return '學號：'.$student_no;
+}) -> where(['student_no' => 's[0-9]{10}']);
+Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
+    return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
+}) -> where(['student_no' => 's[0-9]{10}','subject' => '(chinese|english|math)']);
