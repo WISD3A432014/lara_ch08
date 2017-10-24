94b9a40d84e45608cfe0d7fe501fe7656db4ceb9
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-10-24 17:13:19 +0800

    修改StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,16 +1,15 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 class StudentController extends Controller
 {
-    //練習九: 加入getStudentData及getStudentScore函數
-    public function getStudentData($student_no){
-        return "學號: ".$student_no;
+    public function getStudentData($student_no)
+    {
+        return view::make('score', ['student_no' => $student_no, 'subject' => null
+        ]);
     }
     public function getStudentScore($student_no, $subject = null){
-        return "學號：" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "成績";
+        return view::make('score', ['student_no' => $student_no, 'subject' => $subject
+        ]);
     }
 }
