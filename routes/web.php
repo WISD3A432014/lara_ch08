e7f3effe91760297505844b34a7a8127581542cb
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-27 01:33:52 +0800

    在student資料表內的no資料

--- routes/web.php
+++ routes/web.php
@@ -123,3 +123,9 @@ Route::get('/addscore',function (){
     $score -> total = "180";
     $score -> save();
 });
+Route::get('updatestudent',function(){
+    $student= \App\Student::find(1);
+    $student->update([
+        'no'=>'s1234567890'
+    ]);
+});
