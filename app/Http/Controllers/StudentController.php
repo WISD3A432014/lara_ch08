f834171456939bb8e92431c6bda35b671a1de603
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-27 02:04:33 +0800

    修改 student.blade.php(增加email)、修改Controller(為了獲取$student_no)

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,14 +1,17 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Student;
+use View;
 class StudentController extends Controller
 {
     public function getStudentData($student_no)
     {
 
-        return view::make('score', ['student_no' => $student_no, 'subject' => null
+        return view::make('student', ['student_no' => $student_no, 'subject' => null
         ]);
-        
+
         $student=Student::where('no',$student_no)->firstOrFail();
         return View::make('student',[
             'student'=>$student,
