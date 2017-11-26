a4d13eae0f0d7480799f17f1f921ff3c03682fef
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-27 01:50:38 +0800

    引用 Student model並修改 StudentController 之 getStudentData()方法及getStudentScore()方法

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,11 +5,29 @@ class StudentController extends Controller
 {
     public function getStudentData($student_no)
     {
+        /*
         return view::make('score', ['student_no' => $student_no, 'subject' => null
+        ]);
+        */
+        $student=Student::where('no',$student_no)->firstOrFail();
+        return View::make('student',[
+            'student'=>$student,
+            'user'=>$student->user,
+            'score'=>$student->score,
+            'subject'=>null
         ]);
     }
     public function getStudentScore($student_no, $subject = null){
+        /*
         return view::make('score', ['student_no' => $student_no, 'subject' => $subject
+        ]);
+        */
+        $student=Student::where('no',$student_no)->firstOrFail();
+        return View::make('student',[
+            'student'=>$student,
+            'user'=>$student->user,
+            'score'=>$student->score,
+            'subject'=>$subject
         ]);
     }
 }
