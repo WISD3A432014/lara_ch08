fecb1dcc31ce40dae6764d6c00d89252faff822c
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-27 01:22:40 +0800

    在BoardController一開始的位置先引用Score model

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+
+use App\Score;
+use Route;
+use View;
+
 class BoardController extends Controller
 {
     public function getIndex(){
