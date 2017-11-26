7bcf4cbddcbcee11021fd0cc3d90b076c9cdf2a9
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-27 01:15:26 +0800

    建立student 及 score 兩者間的一對一關係

--- app/Score.php
+++ app/Score.php
@@ -1,7 +1,12 @@
 <?php
 namespace App;
 use Illuminate\Database\Eloquent\Model;
+use \App\Student as StudentEloquent;
 class Score extends Model
 {
-    //
+    protected $table = 'score';
+    //成績屬於一位學生的 (a Score belongTo a Student)
+    public function student(){
+        return $this->belongsTo(StudentEloquent::class);
+    }
 }
--- app/Student.php
+++ app/Student.php
@@ -1,7 +1,13 @@
 <?php
 namespace App;
 use Illuminate\Database\Eloquent\Model;
+
+use \App\Score as ScoreEloquent;
 class Student extends Model
 {
-    //
+    protected $table = 'student';
+    //學生對應到一個成績 (a Student hasOne Score)
+    public function score(){
+        return $this->hasOne(ScoreEloquent::class);
+    }
 }
